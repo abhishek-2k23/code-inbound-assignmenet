863cf2509bbc85398c2f4a31d840111eb91708a0
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-09-06 21:56:47 +0530

    added the componentes with styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import WelcomeScreen from './components/WelcomeScreen';
+import SurveyScreen from './components/SurveyScreen';
+import ThankYouScreen from './components/ThankYouScreen';
 
 const App = () => {
-  return (
-    <div className='bg-red-300'>App</div>
-  )
-}
+  const { isCompleted, welcomeScreen } = useSelector((state) => state.survey);
 
-export default App
+  return (
+    <div className="App bg-blue-500 " >
+      {
+        welcomeScreen ? <WelcomeScreen /> : isCompleted ? <ThankYouScreen /> : <SurveyScreen />
+      }
+    </div>
+  );
+};
+
+export default App;
